bf97e791f18a75fe802e399ecd25c1fd57c1f8ee
joe10111 <41969516+joe10111@users.noreply.github.com>
2019-01-17 19:46:07 -0700

    Fix for previous upload
    Lets have this work

---
+++ Star Traders 2 Alpha Build/Star Traders 2 Alpha Build/Assets/Shooter Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+      private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(gameObject.tag == "Player")
+        {
+            Health.instance.DamageToPlayer();
+        }
+    }
+}
---
+++ Star Traders 2 Alpha Build/Star Traders 2 Alpha Build/Assets/Shooter Assets/Scripts/playerShot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShot : MonoBehaviour
+{
+    public float shotSpeed = 7f;
+    public GameObject impactEffect;
+    public GameObject objectExplostion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += new Vector3(shotSpeed * Time.deltaTime, 0f, 0f);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        if (other.tag == "Space Object")
+        {
+            Instantiate(objectExplostion, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+
+            GameManager.instance.AddScore(50);
+        }
+        if(other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyController>().DamageEnemy();
+        }
+        Destroy(this.gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
