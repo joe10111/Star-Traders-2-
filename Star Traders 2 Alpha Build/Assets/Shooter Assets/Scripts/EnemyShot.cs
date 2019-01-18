f47e3b7e09d15f4701b815de4e16a9c21e65ffd4
joe10111 <41969516+joe10111@users.noreply.github.com>
2019-01-17 18:59:46 -0700

    Star Traders 2 1/17/19
    Moving from hardrive to Git affter power up disater

---
+++ Star Traders 2 Alpha Build/Assets/Shooter Assets/Scripts/EnemyShot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour
+{
+    public float shotSpeed = 7f;
+    public GameObject impactEffect;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(shotSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        if(other.tag == "Player")
+        {
+            Health.instance.DamageToPlayer();
+        }
+        Destroy(this.gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Star Traders 2 Alpha Build/Assets/Shooter Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+
+    public Transform bottomLeftLimit, topRightlimit;
+
+    public Transform shotPoint;
+    public GameObject shot;
+
+    public float timeBetweenShots = 0.1f;
+
+    private float shotCounter;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.position.x, topRightlimit.position.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.position.y, topRightlimit.position.y), transform.position.z);
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(shot, shotPoint.position, shotPoint.rotation);
+            shotCounter = timeBetweenShots;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                Instantiate(shot, shotPoint.position, shotPoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
+    }
+}
