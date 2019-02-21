08ad17c8005a12a06e9726ae560b9596d9530739
LeoSipowicz <46508152+LeoSipowicz@users.noreply.github.com>
2019-02-21 11:05:14 -0800

    MainMenu

---
+++ Star Traders 2 Alpha Build/Star Traders 2 Alpha Build/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Loads current scene plus 1
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    //quits game if built else debug log = "quit'
+    public void ExitGame ()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
