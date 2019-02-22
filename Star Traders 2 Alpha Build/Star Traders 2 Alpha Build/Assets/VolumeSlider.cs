eabd63152fa73cea2a8d189060283648cca6066b
LeoSipowicz <46508152+LeoSipowicz@users.noreply.github.com>
2019-02-22 13:07:09 -0800

    volslide

---
+++ Star Traders 2 Alpha Build/Star Traders 2 Alpha Build/Assets/VolumeSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+
+public class VolumeSlider : MonoBehaviour
+{
+    public Slider Volume;
+    public AudioMixer myMusic;
+
+    public void SoundChange(float soundLevel)
+    {
+        myMusic.SetFloat("masterVol" , soundLevel);
+    }
+}
