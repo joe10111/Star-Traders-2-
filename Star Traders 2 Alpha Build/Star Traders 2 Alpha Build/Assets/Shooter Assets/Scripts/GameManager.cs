f008f8588d8dad2d3dc1829e95f0fb3c5b9d90c9
joe10111 <41969516+joe10111@users.noreply.github.com>
2019-03-18 07:32:32 -0600

    TWeaks

--- Star Traders 2 Alpha Build/Star Traders 2 Alpha Build/Assets/Shooter Assets/Scripts/GameManager.cs
+++ Star Traders 2 Alpha Build/Star Traders 2 Alpha Build/Assets/Shooter Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -26,14 +27,16 @@ public class GameManager : MonoBehaviour
 
     public int levelNumber;
 
+    public Text stageText;
+
     private void Awake()
     {
         instance = this;
     }
-    
-     void Start()
+
+    void Start()
     {
-       
+
         Scene currentScence = SceneManager.GetActiveScene();
         string scenceName = currentScence.name;
         if (scenceName == "Main Menu")
@@ -55,160 +58,190 @@ public class GameManager : MonoBehaviour
             PlayerPrefs.SetInt("LevelNumber", 1);
         }
 
-        
+
         levelNumber = PlayerPrefs.GetInt("LevelNumber");
         //upgrades
         //FireRate
         PlayerController.instance.timeBetweenShots = PlayerPrefs.GetFloat("UpFireRate");
-            //Health
-            Health.instance.maxHealth = PlayerPrefs.GetInt("HealthUp");
-            Health.instance.currentHealth = Health.instance.maxHealth;
-            UIManager.instance.healthBar.maxValue = Health.instance.maxHealth;
-            UIManager.instance.healthBar.value = Health.instance.currentHealth;
-            print(Health.instance.maxHealth);
-            //Speed
-            PlayerController.instance.moveSpeed = PlayerPrefs.GetFloat("SpeedUp");
+        //Health
+        Health.instance.maxHealth = PlayerPrefs.GetInt("HealthUp");
+        Health.instance.currentHealth = Health.instance.maxHealth;
+        UIManager.instance.healthBar.maxValue = Health.instance.maxHealth;
+        UIManager.instance.healthBar.value = Health.instance.currentHealth;
+        print(Health.instance.maxHealth);
+        //Speed
+        PlayerController.instance.moveSpeed = PlayerPrefs.GetFloat("SpeedUp");
 
-            //upgrades
+        //upgrades
 
-            currentLives = PlayerPrefs.GetInt("CurrentLives");
-            UIManager.instance.livesText.text = "X " + currentLives;
-        
-
+        currentLives = PlayerPrefs.GetInt("CurrentLives");
+        UIManager.instance.livesText.text = "X " + currentLives;
 
 
-             highScore = PlayerPrefs.GetInt("HighSCORE");
-            UIManager.instance.highScoreText.text = "HI-SCORE: " + highScore;
 
-            currentScore = PlayerPrefs.GetInt("CurrentScore");
-            UIManager.instance.scoreText.text = "SCORE: " + currentScore;
-        
+
+        highScore = PlayerPrefs.GetInt("HighSCORE");
+        UIManager.instance.highScoreText.text = "HI-SCORE: " + highScore;
+
+        currentScore = PlayerPrefs.GetInt("CurrentScore");
+        UIManager.instance.scoreText.text = "SCORE: " + currentScore;
+
     }
 
-     void Update()
+    void Update()
     {
-        if(levelNumber == 3)
+        if (levelNumber == 3)
         {
+            stageText.text = "STAGE THREE";
             WaveManager.instance.esay = false;
             WaveManager.instance.medium = true;
         }
-
+        if (levelNumber == 4)
+        {
+            stageText.text = "STAGE FOUR";
+        }
         if (levelNumber == 5)
         {
+            stageText.text = "STAGE FIVE";
             WaveManager.instance.esay = false;
             WaveManager.instance.medium = false;
             WaveManager.instance.hard = true;
         }
+        if (levelNumber == 6)
+        {
+            stageText.text = "STAGE SIX";
+        }
+        if (levelNumber == 7)
+        {
+            stageText.text = "STAGE SEVEN";
+        }
+        if (levelNumber == 8)
+        {
+            stageText.text = "STAGE EIGHT";
+        }
+        if (levelNumber == 9)
+        {
+            stageText.text = "STAGE NINE";
+        }
+        if (levelNumber == 10)
+        {
+            stageText.text = "STAGE TEN";
+        }
+        if (levelNumber > 10)
+        {
+            stageText.text = "GALAXYS EDGE";
+        }
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+            {
 
 
-            StartCoroutine(EndLevelCo());
-           
-        }
+                StartCoroutine(EndLevelCo());
 
-        Scene currentScence = SceneManager.GetActiveScene();
-        string scenceName = currentScence.name;
-       
-        
+            }
+
+            Scene currentScence = SceneManager.GetActiveScene();
+            string scenceName = currentScence.name;
+
+
             if (levelEnding)
-        {
-            PlayerController.instance.transform.position += new Vector3(0f, PlayerController.instance.boostSpeed * Time.deltaTime, 0f);
-        }
-
-        if(Input.GetKeyDown(KeyCode.K))
-        {
-            KillPlayer();
-        }
-    }
-    
-    public void KillPlayer()
-    {
-        currentLives--;
-        UIManager.instance.livesText.text = "x " + currentLives;
-        if (currentLives > 0)
-        {
-            //Respawn code
-            StartCoroutine(RespawnCo());
-        }else
-        {
-            if (currentScore >= PlayerPrefs.GetInt("highScore10"))
             {
-                StartCoroutine(NewHighscoreCo());
+                PlayerController.instance.transform.position += new Vector3(0f, PlayerController.instance.boostSpeed * Time.deltaTime, 0f);
             }
-            else
+
+            if (Input.GetKeyDown(KeyCode.K))
             {
-                //GameOver
-                UIManager.instance.gameOverScreen.SetActive(true);
-                WaveManager.instance.canSpawnWaves = false;
-
-                PlayerPrefs.SetInt("HighSCORE", highScore);
-
-                PlayerPrefs.SetInt("score", currentScore);
+                KillPlayer();
             }
         }
-    }
 
-    public IEnumerator NewHighscoreCo()
-    {
-        UIManager.instance.gameOverAndNewHighscore.SetActive(true);
-        WaveManager.instance.canSpawnWaves = false;
-        yield return new WaitForSeconds(5);
-       PlayerPrefs.SetInt("score", currentScore);
-       SceneManager.LoadScene("NameSelect");
-       Debug.Log("newHighScore");
-    }
-    public IEnumerator RespawnCo()
-    {
-        yield return new WaitForSeconds(respawnTime);
-        Health.instance.Respawn();
-
-        WaveManager.instance.continueSpawning();
-    }
-    public void AddScore(int scoreToAdd)
-    {
-        currentScore += scoreToAdd;
-        levelScore += scoreToAdd;
-        UIManager.instance.scoreText.text = "SCORE: " + currentScore;
-
-        if(currentScore > highScore)
+        public void KillPlayer()
         {
-            highScore = currentScore;
-            UIManager.instance.highScoreText.text = "HI-SCORE: " + highScore;
+            currentLives--;
+            UIManager.instance.livesText.text = "x " + currentLives;
+            if (currentLives > 0)
+            {
+                //Respawn code
+                StartCoroutine(RespawnCo());
+            } else
+            {
+                if (currentScore >= PlayerPrefs.GetInt("highScore10"))
+                {
+                    StartCoroutine(NewHighscoreCo());
+                }
+                else
+                {
+                    //GameOver
+                    UIManager.instance.gameOverScreen.SetActive(true);
+                    WaveManager.instance.canSpawnWaves = false;
+
+                    PlayerPrefs.SetInt("HighSCORE", highScore);
+
+                    PlayerPrefs.SetInt("score", currentScore);
+                }
+            }
         }
-    }
 
-    public IEnumerator EndLevelCo()
-    {
-        UIManager.instance.levelEndScreen.SetActive(true);
-        PlayerController.instance.stopMovement = true;
-        levelEnding = true;
-
-        yield return new WaitForSeconds(.5f);
-
-        UIManager.instance.endLevelScore.text = "LEVEL SCORE: " + levelScore;
-        UIManager.instance.endLevelScore.gameObject.SetActive(true);
-
-      yield return new WaitForSeconds(.5f);
-
-        PlayerPrefs.SetInt("CurrentScore", currentScore);
-        UIManager.instance.endCurrentScore.text = "TOTAL SCORE: " + currentScore;
-        UIManager.instance.endCurrentScore.gameObject.SetActive(true);
-
-        if(currentScore == highScore)
+        public IEnumerator NewHighscoreCo()
         {
+            UIManager.instance.gameOverAndNewHighscore.SetActive(true);
+            WaveManager.instance.canSpawnWaves = false;
+            yield return new WaitForSeconds(5);
+            PlayerPrefs.SetInt("score", currentScore);
+            SceneManager.LoadScene("NameSelect");
+            Debug.Log("newHighScore");
+        }
+        public IEnumerator RespawnCo()
+        {
+            yield return new WaitForSeconds(respawnTime);
+            Health.instance.Respawn();
+
+            WaveManager.instance.continueSpawning();
+        }
+        public void AddScore(int scoreToAdd)
+        {
+            currentScore += scoreToAdd;
+            levelScore += scoreToAdd;
+            UIManager.instance.scoreText.text = "SCORE: " + currentScore;
+
+            if (currentScore > highScore)
+            {
+                highScore = currentScore;
+                UIManager.instance.highScoreText.text = "HI-SCORE: " + highScore;
+            }
+        }
+
+        public IEnumerator EndLevelCo()
+        {
+            UIManager.instance.levelEndScreen.SetActive(true);
+            PlayerController.instance.stopMovement = true;
+            levelEnding = true;
+
             yield return new WaitForSeconds(.5f);
-            UIManager.instance.highscoreNotice.SetActive(true);
+
+            UIManager.instance.endLevelScore.text = "LEVEL SCORE: " + levelScore;
+            UIManager.instance.endLevelScore.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(.5f);
+
+            PlayerPrefs.SetInt("CurrentScore", currentScore);
+            UIManager.instance.endCurrentScore.text = "TOTAL SCORE: " + currentScore;
+            UIManager.instance.endCurrentScore.gameObject.SetActive(true);
+
+            if (currentScore == highScore)
+            {
+                yield return new WaitForSeconds(.5f);
+                UIManager.instance.highscoreNotice.SetActive(true);
+            }
+            levelNumber += 1;
+            PlayerPrefs.SetInt("LevelNumber", levelNumber);
+            PlayerPrefs.SetInt("HighSCORE", highScore);
+            PlayerPrefs.SetInt("CurrentLives", currentLives);
+
+            yield return new WaitForSeconds(waitForLevelEnd);
+
+            SceneManager.LoadScene(nextLevel);
+
         }
-        levelNumber += 1;
-        PlayerPrefs.SetInt("LevelNumber", levelNumber);
-        PlayerPrefs.SetInt("HighSCORE", highScore);
-        PlayerPrefs.SetInt("CurrentLives", currentLives);
-
-        yield return new WaitForSeconds(waitForLevelEnd);
-
-        SceneManager.LoadScene(nextLevel);
 
     }
-    
-}
+
